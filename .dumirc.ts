42213ec642aa12e8987cdafbcc86386fa91847d0
hocgin <hocgin@gmail.com>
2023-11-22 16:07:03 +0800

    dev

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'dumi';
+import {defineConfig} from 'dumi';
 
+const repo = 'selector-cursor';
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: '@hocgin/selector-cursor',
   },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
